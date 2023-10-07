f980729592c60bf3684cfba6272e3d8de5e1d45b
Awais Ali <100486479+awaisrehmat@users.noreply.github.com>
2023-10-07 13:12:46 +0500

    Experience Cards

--- src/constants/project.js
+++ src/constants/project.js
@@ -1,13 +1,16 @@
 import ecommerceImg from "../media/ecommerce.png";
 import linkedincloneImg from "../media/linkedinclone.png";
 import restcountriesImg from "../media/restcountries.png";
+import EduGen from "../media/EduGen.png";
+import Mortgage from "../media/Mortgage.png";
+import Expense from "../media/Expense.png"
 
 export const projects = [
   {
     title: "EduGen(FYP)",
     description:
       "This is mobile/web base application to generate AI base Paper generation and custom paper generation",
-    image: linkedincloneImg,
+    image: EduGen,
     tags: ["React", "Express", "MongoDB", "Node"],
     source: "",
     liveVersion: "",
@@ -17,17 +20,17 @@ export const projects = [
     title: "Expense Tracking System",
     description:
       "A fully responsive website, built with Bootstrap, Node, Express",
-    image: ecommerceImg,
+    image: Expense,
     tags: ["HTML", "CSS", "Bootstrap", "Node"],
     source: "",
     liveVersion: "",
     id: 1,
   },
   {
-    title: "Mortage Calculator",
+    title: "Mortgage Calculator",
     description:
       "Resposive Web App, created for claculate Mortage Capacity or borrow capacity",
-    image: restcountriesImg,
+    image: Mortgage,
     tags: ["React", "MUI", "Tailwind CSS"],
     source: "",
     liveVersion: "",
